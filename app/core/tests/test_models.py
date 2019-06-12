a218e3690037c1e8e16c0a32a00e5ed1735a2b5e
harsha <sriharshashm@gmail.com>
2019-06-12 23:44:27 +0530

    [ADD] User Model and tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = "test@test.com"
+        password = "Test123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test for new user email is normalized"""
+        email = "harsha@TEST.com"
+        password = "password"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test checking if invalid email gives an error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test to check if a user is a superuser"""
+        user = get_user_model().objects.create_superuser(
+            email="harsha@test.com",
+            password="password"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
